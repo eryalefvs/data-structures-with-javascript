1fe38436233a6b68cd7ab28a03435cbe7787e4ea
Eryálef Vieira <eryalef@gmail.com>
2024-03-18 23:28:43 -0300

    palindrome check with deque

---
+++ deque/testPalindromeChecker.js
@@ -0,0 +1,7 @@
+import palindromeChecker from "./palindromeChecker.js";
+
+console.log(palindromeChecker("a"));
+console.log(palindromeChecker("aa"));
+console.log(palindromeChecker("level"));
+console.log(palindromeChecker('Was it a car or a cat I saw'));
+console.log(palindromeChecker("true"));
