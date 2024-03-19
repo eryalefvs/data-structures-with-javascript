c6f90bb9ef9041236b12af1a133ac3ad59d2fb49
Eryálef Vieira <eryalef@gmail.com>
2024-03-19 11:21:22 -0300

    class LinkedList

---
+++ linked_list/linkedList.js
@@ -0,0 +1,10 @@
+import { defaultEquals } from "../util/util.js";
+import { Node } from "./models/linked-list-models.js";
+
+export default class LinkedList {
+    constructor(equalsFn = defaultEquals) {
+        this.count = 0;
+        this.head = undefined;
+        this.equalsFn = equalsFn;
+    }
+}
