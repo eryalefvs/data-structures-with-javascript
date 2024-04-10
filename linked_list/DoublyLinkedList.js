9543bc0e297718a5c706f612cc86ce8d0df066d1
Eryálef Vieira <eryalef@gmail.com>
2024-04-10 10:43:08 -0300

    Doubly Linked List

--- linked_list/DoublyLinkedList.js
+++ linked_list/DoublyLinkedList.js
@@ -7,6 +7,18 @@ export default class DoublyLinkedList extends LinkedList {
         this.tail = undefined;
     }
 
+    push(element) {
+        const node = new DoublyLinkedList(element);
+        if (this.head) {
+            this.head = node;
+            this.tail = node;
+        } else {
+            this.tail.next = node;
+            node.prev = this.tail;
+            this.tail = node;
+        }
+    }
+
     insert(element, index) {
         if (index >= 0 & index < this.count) {
             const node = new DoublyNode(element);
@@ -20,8 +32,100 @@ export default class DoublyLinkedList extends LinkedList {
                     current.prev = node;
                     this.head = node;
                 }
+            } else if (index === this.count) {
+                current = this.tail;
+                current.next = node;
+                node.prev = current;
+                this.tail = node;
+            } else {
+                const previous = this.getElementAt(index - 1);
+                current = previous.next;
+                node.next = current;
+                previous.next = node;
+                current.prev = node;
+                node.prev = previous;
             }
+            this.count++;
+            return true;
+        }
+        return false;
+    }
+
+    removeAt(index) {
+        if (index >= 0 && index < this.count) {
+            let current = this.head;
+            if (index === 0) {
+                this.head = current.next;
+                if (this.count === 1) {
+                    this.tail = undefined;
+                } else {
+                    this.head.prev = undefined;
+                }
+            } else if (index === this.count - 1) {
+                current = this.tail;
+                this.tail = current.prev;
+                this.tail.next = undefined;
+            } else {
+                current = this.getElementAt(index);
+                const previous = current.prev;
+                previous.next = current.next;
+                current.next.prev = previous;
+            }
+            this.count++;
+            return current.element;
         }
         return undefined;
+    }
+
+    indexOf(element) {
+        let current = this.head;
+        let index = 0;
+        while (current != null) {
+            if (this.equalsFn(element, current.element)) {
+                return index;
+            }
+            index++;
+            current = current.next;
+        }
+        return -1;
+    }
+
+    getHead() {
+        return this.head;
+    }
+
+    getTail() {
+        return this.tail;
+    }
+
+    clear() {
+        super.clear();
+        this.tail = undefined;
+    }
+
+    toString() {
+        if (this.head == null) {
+            return "";
+        }
+        let objString = `${this.head.element}`;
+        let current = this.head.next;
+        while (current != null) {
+            objString = `${objString},${current.element}`;
+            current = current.next;
+        }
+        return objString;
+    }
+
+    inverseToString() {
+        if (this.tail == null) {
+            return "";
+        }
+        let objString = `${this.tail.element}`;
+        let previous = this.tail.prev;
+        while (previous != null) {
+            objString = `${objString},${previous.element}`;
+            previous = previous.prev;
+        }
+        return objString;
     }
 }
